13db623aaf403e839d9606dcf48007d635362f79
Kelvy Corrêa <79207622+opkwelvy@users.noreply.github.com>
2023-05-10 13:57:12 -0300

    feat: Implementação das Models;

---
+++ Levva.Newbies.Intensivo/Domain/Models/Transacao.cs
@@ -0,0 +1,17 @@
+﻿using Levva.Newbies.Intensivo.Domain.Enums;
+
+namespace Levva.Newbies.Intensivo.Domain.Models
+{
+    public class Transacao
+    {
+        public int Id { get; set; } 
+        public string Descricao { get; set; }  
+        public decimal Valor {get; set; }
+        public DateTime Data { get; set; }
+        public TipoTransacaoEnum Tipo { get; set; }
+        public int CategoriaId { get; set; } 
+        public virtual Categoria Categoria { get; set; }
+        public int UsuarioId { get; set; }
+        public virtual Usuario Usuario { get; set; }
+    }
+}
